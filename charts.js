78bd2497258300f671c01e3900249aa70a3bb7fd
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-12-02 17:55:01 -0500

    Making Changes

--- charts.js
+++ charts.js
@@ -3,12 +3,14 @@ const ctx = document.getElementById('barChart');
         new Chart(ctx, {
             type: 'bar',  // Bar chart
             data: {
-                labels: ["Red", "Blue", "Yellow"],
+                labels: ["lightPink", "mediumPink", "darkPink","mediumDarkPink"],
                 datasets: [
                     {
                         label: "Votes",
-                        data: [12, 9, 3],
-                        backgroundColor: ["#F8C8D1", "#F7A8B8", "#FF66B2"],  // pink shades
+                        data: [12, 9, 3, 6],
+                        backgroundColor: ["#F8C8D1", "#F7A8B8", "#FF66B2", "#FF99CC"],  // pink shades
+                        borderColor: ["#F4A6B3", "#F18C9E", "#FF3385", "#FF66B2"],
+                        borderWidth: 5
                     }
                 ]
             }
